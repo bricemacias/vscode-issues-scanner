8b0481822cb540495d8700424094b26a626c10d0
Brice Mvf <bricemacias@hotmail.com>
2019-12-31 05:31:35 +0100

    Another detail on StyledComponents modified

--- src/theme/StyledComponents.js
+++ src/theme/StyledComponents.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  width: 120%;
+  width: 100%;
   justify-content: center;
   flex-direction: column;
   align-items: center;
